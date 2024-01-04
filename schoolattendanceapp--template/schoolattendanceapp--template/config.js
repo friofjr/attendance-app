17df8e118269aa96f6663bdeac58030f85a947f7
friofjr <117221594+friofjr@users.noreply.github.com>
2024-01-04 20:57:48 +0000

    Add files via upload

---
+++ schoolattendanceapp--template/schoolattendanceapp--template/config.js
@@ -0,0 +1,15 @@
+ import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmSXcofZpEUWld2wfHWKEmDsyA4nlA8fs",
+  authDomain: "project-60-ed9d5.firebaseapp.com",
+  databaseURL: "https://project-60-ed9d5-default-rtdb.firebaseio.com",
+  projectId: "project-60-ed9d5",
+  storageBucket: "project-60-ed9d5.appspot.com",
+  messagingSenderId: "480540904803",
+  appId: "1:480540904803:web:12d026f77713135e631756"
+};
+  export default firebase.database()
+ 
+
+  
